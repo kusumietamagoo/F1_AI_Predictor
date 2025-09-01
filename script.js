353da8eb6713feedc20e68e625bfa8bc696e2f08
KusumietaMagoo <156588684+kusumietamagoo@users.noreply.github.com>
2025-09-01 15:19:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+document.addEventListener("DOMContentLoaded", () => {
+  const sliders = document.querySelectorAll(".tuner-slider");
+
+  sliders.forEach(slider => {
+    slider.addEventListener("input", (e) => {
+      console.log(`${e.target.id} set to: ${e.target.value}`);
+    });
+  });
+});
